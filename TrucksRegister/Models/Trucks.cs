0dba7dee1517ff00e8e6121177a7e86e207573f8
LuizRossa <luizgr20@gmail.com>
2022-12-02 21:44:53 -0300

    Adicionado classe model para a entidade Trucks, e a classe de contexto.

---
+++ TrucksRegister/Models/Trucks.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+namespace TrucksRegister.Models
+{
+    public class Trucks
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int ManufacturingYear { get; set; }
+
+        [Required]
+        public int ModelYear { get; set; }
+    }
+}
---
+++ TrucksRegister/Models/TrucksContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TrucksRegister.Models
+{
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext() { }
+
+        public TrucksContext(DbContextOptions<TrucksContext> options) : base(options) { }
+
+        public DbSet<Trucks> Trucks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("TrucksDatabase"));
+            }
+        }
+    }
+}
